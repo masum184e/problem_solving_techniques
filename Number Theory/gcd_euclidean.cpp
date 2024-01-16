1ac3438608f1b3c14f5768c184faef2dc9a0ce73
masum184e <masum184e@gmail.com>
2024-01-16 08:06:33 +0600

    recursive & iterative updated

--- Number Theory/gcd_euclidean.cpp
+++ Number Theory/gcd_euclidean.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 #define ll long long int
 
-int gcd(int a, int b){
+ll gcd_euclidean_recursive(ll a, ll b){
   // A IS GREATER THAN B
   if(a==0)return b;
-  return gcd(b%a,a);
+  return gcd_euclidean_recursive(b%a,a);
 }
 
-// ll gcd(ll a, ll b){
-//   // A IS GREATER THAN B
-//   while(b!=0){
-//     ll r=a%b;
-//     a=b;
-//     b=r;
-//   }
-//   return a;
-// }
+ll gcd_euclidean_iterative(ll a, ll b){
+  // A IS GREATER THAN B
+  while(b!=0){
+    ll r=a%b;
+    a=b;
+    b=r;
+  }
+  return a;
+}
 
 int main(){
   // ios_base::sync_with_stdio(false);cin.tie(NULL);
@@ -26,6 +26,7 @@ int main(){
   cin>>a;
   cout<<"Enter smaller number: ";
   cin>>b;
-  cout<<"GCD is: "<<gcd(a,b)<<endl;
+  cout<<"GCD is(recursive): "<<gcd_euclidean_recursive(a,b)<<endl;
+  cout<<"GCD is(iterative): "<<gcd_euclidean_iterative(a,b)<<endl;
   return 0;
 }
